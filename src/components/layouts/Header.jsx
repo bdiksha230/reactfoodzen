355523181fbce01af34cdad110b581226055af23
bdiksha230 <90504015+bdiksha230@users.noreply.github.com>
2024-01-10 12:53:55 +0530

    changes in home path

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -24,7 +24,7 @@ const Header = () => {
         <header className={isSticky ? 'sticky' : ''}>
             <nav className="navbar navbar-expand-lg  ">
                 <div className="container">
-                    <NavLink to="/" className="navbar-brand logo" >FoodZen</NavLink>
+                    <NavLink to="/Home" className="navbar-brand logo" >FoodZen</NavLink>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
